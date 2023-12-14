0c02771ac3cd237207ac6498066203981e0bf6e6
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-14 12:34:50 +0100

    Handle the night theme
    If it's night at the moment of search in the searched city, the
    background and header sticker will be accordingly switched to the
    night theme.

--- src/display.js
+++ src/display.js
@@ -114,5 +114,16 @@ export default async function displayWeatherData(city) {
 		dialog.style.display = 'flex';
 		displayWeatherData('London');
 	} else {
+		headerSticker.src = sun;
+
+		if (!data.isDayAtTheMoment) {
+			body.style.background =
+				'url(https://images.unsplash.com/photo-1507754988072-8667848efc79?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)';
+
+			body.style.backgroundSize = 'cover';
+			body.style.backgroundPosition = 'bottom';
+
+			headerSticker.src = moon;
+		}
 	}
 }
